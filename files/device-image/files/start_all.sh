e9153d930f7699174f1ba4096817c4fde3a04360
Aliaksei Khursevich <hursrevich@gmail.com>
2017-10-24 19:55:32 +0300

    - externalization of stf configs

--- files/device-image/files/start_all.sh
+++ files/device-image/files/start_all.sh
@@ -7,6 +7,6 @@ ln -s /usr/lib/jvm/java-8-openjdk-amd64/bin/java /usr/bin/java \
     & node /opt/appium/ -p $PORT --log-timestamp --session-override --udid $DEVICEUDID \
            --nodeconfig /opt/nodeconfig.json --automation-name $AUTOMATION_NAME \
     & stf provider --name "$DEVICENAME-container" --min-port=$MIN_PORT --max-port=$MAX_PORT \
-        --connect-sub tcp://213.184.251.86:7114 --connect-push tcp://213.184.251.86:7116 \
-        --group-timeout 3600 --public-ip smule.qaprosoft.com --storage-url http://smule.qaprosoft.com/ \
+        --connect-sub tcp://$STF_PRIVATE_HOST:$STF_TCP_SUB_PORT --connect-push tcp://$STF_PRIVATE_HOST:$STF_TCP_PUB_PORT \
+        --group-timeout 3600 --public-ip $STF_PUBLIC_HOST --storage-url http://$STF_PUBLIC_HOST/ \
         --vnc-initial-size 600x800 --no-cleanup
