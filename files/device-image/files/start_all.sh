1acf4112fa3f6cbc0774d9d9bccacecf51cc883a
vdelendik <vadim@onflydev.com>
2017-10-26 09:49:37 -0400

    reuse ansiblefunctionality correctly

--- files/device-image/files/start_all.sh
+++ files/device-image/files/start_all.sh
@@ -6,7 +6,7 @@ ln -s /usr/lib/jvm/java-8-openjdk-amd64/bin/java /usr/bin/java \
     & /opt/configgen.sh > /opt/nodeconfig.json \
     & node /opt/appium/ -p $PORT --log-timestamp --session-override --udid $DEVICEUDID \
            --nodeconfig /opt/nodeconfig.json --automation-name $AUTOMATION_NAME \
-    & stf provider --name "$DEVICENAME-container" --min-port=$MIN_PORT --max-port=$MAX_PORT \
+    & stf provider --name "$DEVICEUDID" --min-port=$MIN_PORT --max-port=$MAX_PORT \
         --connect-sub tcp://$STF_PRIVATE_HOST:$STF_TCP_SUB_PORT --connect-push tcp://$STF_PRIVATE_HOST:$STF_TCP_PUB_PORT \
-        --group-timeout 3600 --public-ip $STF_PUBLIC_HOST --storage-url http://$STF_PUBLIC_HOST/ \
-        --vnc-initial-size 600x800 --no-cleanup
+        --group-timeout 3600 --public-ip $STF_PUBLIC_HOST --storage-url https://$STF_PUBLIC_HOST/ \
+	--heartbeat-interval 10000 --vnc-initial-size 600x800 --no-cleanup --screen-ws-url-pattern "wss://${STF_PUBLIC_HOST}/d/${STF_PRIVATE_HOST}/<%= serial %>/<%= publicPort %>/"
