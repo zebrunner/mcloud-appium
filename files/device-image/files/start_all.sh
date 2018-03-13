af6ad970dbc77224443dc5c87c13a4ebb9f0e1e7
build <hursevich@gmail.com>
2018-03-13 20:21:54 +0300

    added heartbeat_interval feature

--- files/device-image/files/start_all.sh
+++ files/device-image/files/start_all.sh
@@ -9,4 +9,17 @@ ln -s /usr/lib/jvm/java-8-openjdk-amd64/bin/java /usr/bin/java \
     & stf provider --name "$DEVICEUDID" --min-port=$MIN_PORT --max-port=$MAX_PORT \
         --connect-sub tcp://$STF_PRIVATE_HOST:$STF_TCP_SUB_PORT --connect-push tcp://$STF_PRIVATE_HOST:$STF_TCP_PUB_PORT \
         --group-timeout 3600 --public-ip $STF_PUBLIC_HOST --storage-url https://$STF_PUBLIC_HOST/ \
-	--heartbeat-interval 10000 --vnc-initial-size 600x800 --no-cleanup --screen-ws-url-pattern "wss://${STF_PUBLIC_HOST}/d/${STF_PRIVATE_HOST}/<%= serial %>/<%= publicPort %>/"
+	--heartbeat-interval 10000 --vnc-initial-size 600x800 --no-cleanup --screen-ws-url-pattern "wss://${STF_PUBLIC_HOST}/d/${STF_PRIVATE_HOST}/<%= serial %>/<%= publicPort %>/" & 
+
+while true
+  do
+    sleep $HEARTBEAT_INTERVAL
+    ADB_STATUS=$(adb devices | grep -c ${DEVICEUDID})
+    if [ ! "$ADB_STATUS" -eq "1" ]; then
+        echo adb server is dead. restarting container...
+        exit -1
+    else
+        echo adb service is alive.
+    fi
+  done
+
