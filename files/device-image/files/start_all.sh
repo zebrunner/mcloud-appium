d3862b4ff4754ff18b7c6ea6beef4526d21ff49e
vdelendik <vdelendik@qaprosoft.com>
2018-06-26 18:01:28 +0300

    secure variant for websockify startup

--- files/device-image/files/start_all.sh
+++ files/device-image/files/start_all.sh
@@ -6,7 +6,7 @@ ln -s /usr/lib/jvm/java-8-openjdk-amd64/bin/java /usr/bin/java \
     & /opt/configgen.sh > /opt/nodeconfig.json \
     & node /opt/appium/ -p $PORT --log-timestamp --session-override --udid $DEVICEUDID \
            --nodeconfig /opt/nodeconfig.json --automation-name $AUTOMATION_NAME \
-    & /opt/websockify/run ${MAX_PORT} :5900 \
+    & /opt/websockify/run ${MAX_PORT} :5900  --ssl-only --cert /opt/nginx/ssl/ssl.crt --key /opt/nginx/ssl/ssl.key \
     & stf provider --name "$DEVICEUDID" --min-port=$MIN_PORT --max-port=$MAX_PORT \
         --connect-sub tcp://$STF_PRIVATE_HOST:$STF_TCP_SUB_PORT --connect-push tcp://$STF_PRIVATE_HOST:$STF_TCP_PUB_PORT \
         --group-timeout 3600 --public-ip $STF_PUBLIC_HOST --storage-url https://$STF_PUBLIC_HOST/ \
